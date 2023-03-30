d4243c80f97c6164ddb87488086a45015381cd2c
v.bratusevs <brvl92@gmail.com>
2023-03-30 22:16:29 +0100

    [master]: Project upload (2)

--- src/test/java/StudentAppTest.java
+++ src/test/java/StudentAppTest.java
@@ -1,18 +1,35 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class StudentAppTest {
-
-    @Test
-    public void openStudentApp() {
+    WebDriver driver;
+    private final String APP_URL = "http://app.acodemy.lv/";
+    @BeforeMethod
+    public void initialize() {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
-        WebDriver driver = new ChromeDriver(options);
-        driver.get("http://app.acodemy.lv/");
+        driver = new ChromeDriver(options);
+        driver.get(APP_URL);
+    }
+    @AfterMethod
+    public void tearDown() {
         driver.close();
         driver.quit();
     }
-
+    @Test
+    public void openStudentApp() {
+        WebElement addStudentButton = driver.findElement(By.xpath("//div[@class='ant-table-title']//button"));
+        addStudentButton.click();
+        WebElement nameField = driver.findElement(By.id("name"));
+        nameField.sendKeys("Vlad");
+        WebElement emailField = driver.findElement(By.id("email"));
+        emailField.sendKeys("123@gmail.com");
+  //      driver.findElement(By.xpath("//div[@class='ant-form-item-control-input-content']//button")).click();
+    }
 }
