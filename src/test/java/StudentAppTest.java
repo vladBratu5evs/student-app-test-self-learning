30d950ee28729c0c80b00c3eda26220261cf95d6
v.bratusevs <brvl92@gmail.com>
2023-03-30 21:09:38 +0100

    [master]: Project upload

---
+++ src/test/java/StudentAppTest.java
@@ -0,0 +1,18 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Test;
+
+public class StudentAppTest {
+
+    @Test
+    public void openStudentApp() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        WebDriver driver = new ChromeDriver(options);
+        driver.get("http://app.acodemy.lv/");
+        driver.close();
+        driver.quit();
+    }
+
+}
