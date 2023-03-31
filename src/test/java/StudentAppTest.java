4e0c92a98aa60e46079b6a47e6790c0e8468f53a
v.bratusevs <brvl92@gmail.com>
2023-03-31 20:40:10 +0100

    [master]: Project upload (3)

--- src/test/java/StudentAppTest.java
+++ src/test/java/StudentAppTest.java
@@ -1,20 +1,29 @@
+import com.github.javafaker.Faker;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.time.Duration;
+
 public class StudentAppTest {
     WebDriver driver;
+    WebDriverWait driverWait;
+    Faker dataFaker = new Faker();
     private final String APP_URL = "http://app.acodemy.lv/";
     @BeforeMethod
     public void initialize() {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
         driver = new ChromeDriver(options);
+        driverWait = new WebDriverWait(driver, Duration.ofSeconds(5));
         driver.get(APP_URL);
     }
     @AfterMethod
@@ -24,12 +33,41 @@ public class StudentAppTest {
     }
     @Test
     public void openStudentApp() {
+        driverWait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@class='ant-table-title']//button")));
         WebElement addStudentButton = driver.findElement(By.xpath("//div[@class='ant-table-title']//button"));
         addStudentButton.click();
+
+        driverWait.until(ExpectedConditions.visibilityOfElementLocated(By.id("name")));
         WebElement nameField = driver.findElement(By.id("name"));
-        nameField.sendKeys("Vlad");
+        String name = dataFaker.harryPotter().character();
+        nameField.sendKeys(name);
+
         WebElement emailField = driver.findElement(By.id("email"));
-        emailField.sendKeys("123@gmail.com");
-  //      driver.findElement(By.xpath("//div[@class='ant-form-item-control-input-content']//button")).click();
+        String email = dataFaker.internet().emailAddress();
+        emailField.sendKeys(email);
+
+        WebElement genderField = driver.findElement(By.id("gender"));
+        genderField.click();
+        driverWait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@title='OTHER']")));
+        WebElement valueFromDropdownValues = driver.findElement(By.xpath("//div[@title='OTHER']"));
+        valueFromDropdownValues.click();
+
+        WebElement submitButton = driver.findElement(By.xpath("//div[@class='ant-form-item-control-input-content']//button"));
+        submitButton.click();
+
+        driverWait.until(ExpectedConditions.visibilityOfElementLocated(By.className("ant-notification-notice-message")));
+        WebElement notificationMessage = driver.findElement(By.className("ant-notification-notice-message"));
+        WebElement notificationDescription = driver.findElement(By.className("ant-notification-notice-description"));
+
+        Assert.assertEquals(notificationMessage.getText(), "Student successfully added" );
+        Assert.assertEquals(notificationDescription.getText(), name + " was added to the system" );
+
+
+
+       driverWait.until(ExpectedConditions.elementToBeClickable(By.className("ant-notification-close-x")));
+       WebElement popUpCloseButton = driver.findElement(By.className("ant-notification-close-x"));
+       popUpCloseButton.click();
+
+       Assert.assertTrue(driverWait.until(ExpectedConditions.invisibilityOf(popUpCloseButton)));
     }
 }
